21256957f537002ddbdf2e81de606175ca0c0885
Daecheol <puri8467@gmail.com>
2016-05-22 17:14:01 +0900

    game, 1번째

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-
+    url(r'^$', views.post_main, name='post_main'),
+    url(r'^explanation', views.post_explain, name='post_explain'),
+    url(r'main', views.post_main, name='post_main'),
+    url(r'game', views.post_game, name='post_game'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-def post_list(request):
-    return render(request, 'game/post_list.html', {})
+def post_main(request):
+    return render(request, 'game/main.html', {})
+
+def post_explain(request):
+    return render(request, 'game/explain.html', {})
+def post_game(request):
+    return render(request, 'game/game.html', {})
 # Create your views here.
